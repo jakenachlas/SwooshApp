9734d55520691de276b16dd5c0e0fdd754e4de58
Jake Nachlas <jakenachlas@gmail.com>
2018-06-21 10:44:32 -0400

    lesson-7

---
+++ app/src/main/java/com/example/jakenachlas/swoosh/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.jakenachlas.swoosh
+
+
+//Constant Immutable Variables
+//Common to use all caps for constants
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/example/jakenachlas/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/jakenachlas/swoosh/LeagueActivity.kt
@@ -4,16 +4,47 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
+    fun onMensClicked(view: View) {
+        womensBtn.isChecked = false
+        coedBtn.isChecked = false
+
+        selectedLeague = "Mens"
+    }
+
+    fun onWomensClicked(view: View ){
+        mensBtn.isChecked = false
+        coedBtn.isChecked = false
+
+        selectedLeague = "Womens"
+    }
+
+    fun onCoedClicked(view: View){
+        mensBtn.isChecked = false
+        womensBtn.isChecked = false
+
+        selectedLeague = "Co-Ed"
+    }
+
+
     fun leagueNextClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
