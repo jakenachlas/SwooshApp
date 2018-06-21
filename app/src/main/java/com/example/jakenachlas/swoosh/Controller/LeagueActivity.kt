28636ea8ec4df01897d6eef674e952e49520f7ac
Jake Nachlas <jakenachlas@gmail.com>
2018-06-21 12:52:17 -0400

    lesson 9

--- app/src/main/java/com/example/jakenachlas/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/jakenachlas/swoosh/Controller/LeagueActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.jakenachlas.swoosh.Utilities.EXTRA_LEAGUE
+import com.example.jakenachlas.swoosh.Model.Player
 import com.example.jakenachlas.swoosh.R
+import com.example.jakenachlas.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+    //var selectedLeague = ""
+    var player = Player("", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,28 +23,28 @@ class LeagueActivity : BaseActivity() {
         womensBtn.isChecked = false
         coedBtn.isChecked = false
 
-        selectedLeague = "Mens"
+        player.league = "mens"
     }
 
     fun onWomensClicked(view: View ){
         mensBtn.isChecked = false
         coedBtn.isChecked = false
 
-        selectedLeague = "Womens"
+        player.league = "womens"
     }
 
     fun onCoedClicked(view: View){
         mensBtn.isChecked = false
         womensBtn.isChecked = false
 
-        selectedLeague = "Co-Ed"
+        player.league = "co-ed"
     }
 
 
     fun leagueNextClicked(view: View) {
-        if (selectedLeague != "") {
+        if (player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         } else {
             Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/jakenachlas/swoosh/Utilities/Constants.kt
+++ app/src/main/java/com/example/jakenachlas/swoosh/Utilities/Constants.kt
@@ -3,5 +3,4 @@ package com.example.jakenachlas.swoosh.Utilities
 
 //Constant Immutable Variables
 //Common to use all caps for constants
-const val EXTRA_LEAGUE = "league"
-const val EXTRA_SKILL = "skill"
+const val EXTRA_PLAYER = "player"
